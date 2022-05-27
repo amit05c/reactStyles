147e1cdd2d65e1584b66d4774807df344dd0d05a
amit05c <101566257+amit05c@users.noreply.github.com>
2022-05-27 22:21:43 +0530

    conditional css

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 // import "./styles.css";
 // import styles from "./App.module.css";
 import styled from "styled-components";
+import { useState } from "react";
 
 const RedColorStyled = styled.div`
   background-color: ${(props) => props.color};
+  /* color: ${(props) => props.colors}; */
 `;
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <RedColorStyled color="green">
+    <RedColorStyled color={count > 5 ? "blue" : "white"}>
       <div className="App">
         {/* <div className="App" style={{ backgroundColor: "red" }}> */}
         {/* <h1 className={styles.head}>Hello CodeSandbox</h1> */}
-        <h1>Hello CodeSandbox</h1>
+        <h1 onClick={() => setCount(count + 1)}>Hello CodeSandbox</h1>
         <h2>Start editing to see some magic happen!</h2>
+        <div>{count}</div>
       </div>
     </RedColorStyled>
   );
