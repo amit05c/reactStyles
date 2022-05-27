5e3cd7a39dcd3447a4ae0f76a5b32e2d516853bd
amit05c <101566257+amit05c@users.noreply.github.com>
2022-05-27 18:40:57 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import styles from "./App.module.css";
+
+export default function App() {
+  return (
+    <div className="App" style={{ backgroundColor: "red" }}>
+      <h1 className={styles.head}>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
