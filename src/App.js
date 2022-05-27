a0bf72466d760ceb2e65789da51a029b1a05233a
amit05c <101566257+amit05c@users.noreply.github.com>
2022-05-27 19:20:16 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import "./styles.css";
-import styles from "./App.module.css";
+// import "./styles.css";
+// import styles from "./App.module.css";
+import styled from "styled-components";
+
+const RedColorStyled = styled.div`
+  background-color: black;
+  color: white;
+`;
 
 export default function App() {
   return (
-    <div className="App" style={{ backgroundColor: "red" }}>
-      <h1 className={styles.head}>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <RedColorStyled>
+      <div className="App">
+        {/* <div className="App" style={{ backgroundColor: "red" }}> */}
+        {/* <h1 className={styles.head}>Hello CodeSandbox</h1> */}
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+      </div>
+    </RedColorStyled>
   );
 }
